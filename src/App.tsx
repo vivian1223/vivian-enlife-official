93fb7696a85b9edc8eecf577c2b25a758bac07a8
Vivian Yeh <u4h93u6uhu@gmail.com>
2024-01-01 08:11:11 +0800

    feat: update subscription plan

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 				className={styles.button}
 				iconClassname={styles.icon}
 				size="small"
-				text="限時 150$ 解鎖訂閱專屬職涯專欄"
+				text="永久會員訂閱方案限時開放中"
 				startIcon={<FontAwesomeIcon className={styles.icon} icon={faArrowRight as IconProp} />}
 				onClick={() => window.open('https://vocus.cc/escapeplan/introduce', '_blank', 'noreferrer')}
 			/>
